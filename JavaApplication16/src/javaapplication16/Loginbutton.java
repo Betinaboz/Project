53f7a0976d31a5498a24eafb4d68207e25af4dfe
blagovestadb <blagovestadb@LAB27-14.school.npmg.org>
2022-12-20 16:23:47 +0200

    lol

--- JavaApplication16/src/javaapplication16/Loginbutton.java
+++ JavaApplication16/src/javaapplication16/Loginbutton.java
@@ -8,48 +8,51 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.WindowConstants;
 
 /**
  *
  * @author Blagovesta
  */
 public class Loginbutton implements ActionListener {
-    private static JButton button; 
-    
+
+    private static JButton button;
+
     public JButton setupLoginbutton() {
-     button=new JButton("Login");
-      button.setBounds(70,220,100,25);
-      button.setFont(new Font("Courier New", Font.PLAIN, 18));
-      button.addActionListener(new Menu());
-      return button;
+        button = new JButton("Login");
+        button.setBounds(70, 220, 100, 25);
+        button.setFont(new Font("Courier New", Font.PLAIN, 18));
       
-    
-}
- 
+        button.addActionListener(new Loginbutton());
+        return button;
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-      
-       Menu o = new Menu();
+     Game g=new Game();
+     Menu o = new Menu();
        String text=o.getUserText().getText();
        
       boolean match=text.matches("[a-zA-Z]+");
        if (match==true){
-           if(o.getEasy().isSelected()){
+       
+     if(o.getEasy().isSelected()){
       o.setWhichLevel(1);
-      System.out.println("raboti");
       }
       else if (o.getMedium().isSelected()){
       o.setWhichLevel(2);
       }
       else if(o.getHard().isSelected()){
           o.setWhichLevel(3);
-      }
-      else System.out.println("Select level");
-       }
-       else {
+      } 
+     
+       g.setupGame();
+           
+    } 
+   else {
            throw new java.util.InputMismatchException("Enter letters!");
-            
-       } 
-      }
-   
+    }
+
+    }
 }
