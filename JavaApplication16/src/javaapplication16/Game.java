e593b2f5e44f1b8214836c1c859fc3ec98adc559
viktoriyana <viktoriyana@LAB27-13.school.npmg.org>
2022-12-22 18:43:56 +0200

    new panel

--- JavaApplication16/src/javaapplication16/Game.java
+++ JavaApplication16/src/javaapplication16/Game.java
@@ -4,19 +4,22 @@
  */
 package javaapplication16;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 
 /**
  *
  * @author Blagovesta
  */
-public class Game {
+public class Game implements ActionListener{
     final int tileHeigth=20;
     final int tileWidth=20;
     private static int width;
     private static int heigth;
     private static int bombCount;
+    private static int flags;
     
     public void setWidth(int width){
         if(width>0)
@@ -49,18 +52,18 @@ public class Game {
         
         switch (new Menu().getWhichLevel()){
             case 1: 
-                heigth=10;
-                width=10;
+                heigth=20;
+                width=20;
                 bombCount=10;
                 break;
             case 2:
-                heigth=15;
-                width=15;
+                heigth=25;
+                width=25;
                 bombCount=15;
                 break;
             case 3:
-                heigth=20;
-                width=20;
+                heigth=35;
+                width=35;
                 bombCount=20;
                 break;
         }
@@ -71,16 +74,79 @@ public class Game {
         
       setLevel();
      JFrame gameFrame=new JFrame();
+     gameFrame.setSize(tileWidth*width,(tileHeigth*heigth)+70);
+     gameFrame.setLayout(null);
+     gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     
+     
       JPanel gamePanel=new JPanel();
-      gameFrame.setSize(tileWidth*width,tileHeigth*heigth);
-      gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      gameFrame.add(gamePanel);
       gamePanel.setLayout(null);
+      gamePanel.setBounds(0,70, tileWidth*width, tileHeigth*heigth);
       gamePanel.setBackground(new java.awt.Color(211, 234, 245));
-        System.out.println(width);
-        System.out.println(heigth);
+     
+      
+      JPanel stats=new JPanel();
+      stats.setLayout(null);
+      stats.setBounds(0,0, tileWidth*width , 70);
+      stats.setBackground(new java.awt.Color(130, 177, 201));
+      // gameFrame.add(stats);
+     // stats.setSize(tileWidth*width,100);
+     // gamePanel.setSize(tileWidth*width,tileHeigth*heigth);
+      gameFrame.add(stats);
+      gameFrame.add(gamePanel); 
+     
+     
+       flags=bombCount;
+       boolean gameLost=false;
+       boolean gameWon=false;
+       
+       
       
        gameFrame.setVisible(true);
       
 }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
