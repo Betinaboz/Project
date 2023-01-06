1b6b9fd26e57050d4e1021b5794e6251c33cacca
Blagovesta <Blagovesta@DESKTOP-BDD3515>
2023-01-06 12:34:29 +0200

    stack overflow

--- JavaApplication16/src/javaapplication16/revealButtons.java
+++ JavaApplication16/src/javaapplication16/revealButtons.java
@@ -4,6 +4,7 @@
  */
 package javaapplication16;
 
+import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -18,37 +19,25 @@ public class revealButtons extends Game implements ActionListener {
     private int bombArray[][];
     private JButton buttonsArray[][];
     
+    public JButton setupReveal(){
+        for (int i = 0; i < b.getWidth(); i++) {
+            for (int j = 0; j < b.getWidth(); j++) {
+                buttonsArray[i][j].addActionListener(new revealButtons());
+                return buttonsArray[i][j];
+            }
+        }
+        return null;
+    }
+    
     
     @Override
     public void actionPerformed(ActionEvent e) {
         b.setLevel();
         b.setArray(bombArray);
-        
-        for(int i=0;i<b.getWidth();i++){
-             for(int j=0;j<b.getHeigth();j++){
-                 /*if(bombArray[i][j]==9)
-                     buttons[i][j].setIcon(mineImageIcon);
-                 if(bombArray[i][j]==0)
-                     buttons[i][j].setIcon(blankImageIcon);
-                 if(bombArray[i][j]==1)
-                     buttons[i][j].setIcon(oneImageIcon);
-                  if(bombArray[i][j]==2)
-                     buttons[i][j].setIcon(twoImageIcon);
-                  if(bombArray[i][j]==3)
-                     buttons[i][j].setIcon(threeImageIcon);
-                  if(bombArray[i][j]==4)
-                     buttons[i][j].setIcon(fourImageIcon);
-                  if(bombArray[i][j]==5)
-                     buttons[i][j].setIcon(fiveImageIcon);
-                  if(bombArray[i][j]==6)
-                     buttons[i][j].setIcon(sixImageIcon);
-                  if(bombArray[i][j]==7)
-                     buttons[i][j].setIcon(sevenImageIcon);
-                  if(bombArray[i][j]==8)
-                     buttons[i][j].setIcon(eigthImageIcon);
-                 
-             }*/
+        b.setButtons(buttonsArray);
+        b.attachImg();
 }
+
+    
 }
-}
-}
+
