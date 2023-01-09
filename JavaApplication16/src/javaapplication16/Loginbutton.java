221cd9fad1b7c6379766202408ba658a86fa6075
Blagovesta <Blagovesta@DESKTOP-BDD3515>
2023-01-09 23:57:06 +0200

    file

--- JavaApplication16/src/javaapplication16/Loginbutton.java
+++ JavaApplication16/src/javaapplication16/Loginbutton.java
@@ -15,8 +15,27 @@ import javax.swing.JButton;
  * @author Blagovesta
  */
 public class Loginbutton implements ActionListener {
-
+    private static String level;
+    private static String text;
     private static JButton button;
+    
+  
+
+    public String getLevel(){
+        return level;
+    }
+    
+    public void setName(String name){
+        if (name != null)
+            this.text= name;
+    }
+    
+    public void setLevel (String level){
+        if (level != null)
+            this.level= level;
+    }
+    
+    
 
     public JButton setupLoginbutton() {
         button = new JButton("Login");
@@ -32,19 +51,24 @@ public class Loginbutton implements ActionListener {
     public void actionPerformed(ActionEvent e) {
      Game g=new Game();
      Menu o = new Menu();
-       String text=o.getUserText().getText();
+     
+       text=o.getUserText().getText();
        
-      boolean match=text.matches("[a-zA-Z]+");
+      boolean match=text.matches("^[A-Za-z](?=.{1,20}$)[A-Za-z]*(?:\\h+[A-Za-z]*)*$"); //позволява на потребителя да въведе име, което да
+     // съдържа само малки и големи букви и интервал до 20 на брой 
        if (match==true){
        
      if(o.getEasy().isSelected()){
       o.setWhichLevel(1);
+      level ="Easy";
       }
       else if (o.getMedium().isSelected()){
       o.setWhichLevel(2);
+      level ="Medium";
       }
       else if(o.getHard().isSelected()){
           o.setWhichLevel(3);
+          level ="Hard";
       } 
      
        g.setupGame();
@@ -57,4 +81,6 @@ public class Loginbutton implements ActionListener {
     }
 
     }
+
+   
 }
